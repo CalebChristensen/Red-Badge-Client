421780b301b6b5c875483273374eb3f6a9ec3081
Caleb Christensen <Caleb.Christ@gmail.com>
2018-10-30 15:07:52 -0400

    first get request

---
+++ src/app/services/poi.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PoiService {
+
+  constructor(public http: HttpClient) {
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('x-api-key', '3XARRaE3NR4VdZpUIAgwO3wJ7I1mnDyp1WQK6CSO');
+    console.log('Hello POI Provider');
+  }
+
+  getPoi() {
+    return this.http.get(`https://api.sygictravelapi.com/1.1/en/places/list?parents=city:16&categories=sightseeing&limit=10`, {
+      headers: new HttpHeaders().set('x-api-key', '3XARRaE3NR4VdZpUIAgwO3wJ7I1mnDyp1WQK6CSO')
+    })
+  }
+
+  getMedia() {
+    return this.http.get(`https://api.sygictravelapi.com/1.1/en/places/poi:530/media`, {
+      headers: new HttpHeaders().set('x-api-key', '3XARRaE3NR4VdZpUIAgwO3wJ7I1mnDyp1WQK6CSO')
+    })
+  }
+}
