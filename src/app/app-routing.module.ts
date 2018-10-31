f3e13947b3928be9e9d31b2cfd02de73bb65fa8c
Justin Weiskittel <justinweiskittel@gmail.com>
2018-10-31 16:27:06 -0400

    new stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component'; 
 import { PoiComponent } from './poi/poi.component';
+import { AccountComponent } from './account/account.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -12,7 +13,8 @@ const routes: Routes = [
   {path: 'about', component: AboutComponent},
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
-  {path: 'poi', component: PoiComponent}
+  {path: 'poi', component: PoiComponent},
+  {path: 'account', component: AccountComponent}
 ];
 
 @NgModule({
