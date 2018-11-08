12e63f464451d810c762ce102614f48677a35d7e
Caleb Christensen <Caleb.Christ@gmail.com>
2018-11-08 16:42:38 -0500

    edits

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,12 @@ export const environment = {
   APIKEY: '3XARRaE3NR4VdZpUIAgwO3wJ7I1mnDyp1WQK6CSO'
 };
 
-export let APIURL = ''
+export let APIURL = '';
 
 switch (window.location.hostname) {
   case 'no-borders-client.herokuapp.com':
-    APIURL = 'https://red-badge-server.herokuapp.com'
+    APIURL = `https://red-badge-server.herokuapp.com`
     break
   default: 
-    APIURL = 'http://localhost:3000'
+    APIURL = `https://red-badge-server.herokuapp.com`
 }
