4e2c5f68523fbeacbba6fce9597ae395e6e584aa
Justin Weiskittel <justinweiskittel@gmail.com>
2018-11-13 09:30:03 -0500

    asdf

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -17,7 +17,7 @@ export class NoteService {
   constructor(private http: HttpClient) { }
 
   addNote(note: string) {
-    return this.http.post<Note>(`https://red-badge-server.herokuapp.com/notes/create`, {note: note}, httpOptions)
+    return this.http.post<Note>(`${APIURL}/notes/create`, {note: note}, httpOptions)
   }
 
   deleteNote(id) {
